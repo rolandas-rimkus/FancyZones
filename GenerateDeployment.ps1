3e6606d50e2a23a3786d2e8ce7a60cc9d30bfd18
Rolandas Rimkus <58506999+rolandas-rimkus@users.noreply.github.com>
2024-01-13 00:15:34 +0200

    Update GenerateDeployment.ps1 to be able to rerun the script in case one of the steps fail
    Updated to latest version of PowerToys to get FancyZones changes

--- GenerateDeployment.ps1
+++ GenerateDeployment.ps1
@@ -3,138 +3,79 @@
 #
 $ErrorActionPreference = 'Stop'
 $ProgressPreference = 'SilentlyContinue'
-
-Write-Host "[INFO] Generating temp directory"
-mkdir "$PSScriptRoot\temp" | Out-Null
-
-Write-Host "[INFO] Downloading Wix"
-Invoke-RestMethod "https://github.com/wixtoolset/wix3/releases/download/wix3112rtm/wix311-binaries.zip" -OutFile "$PSScriptRoot\temp\wix.zip"
-
-Write-Host "[INFO] Extracting Wix"
 Add-Type -Assembly "System.IO.Compression.Filesystem"
-[System.IO.Compression.ZipFile]::ExtractToDirectory("$PSScriptRoot\temp\wix.zip", "$PSScriptRoot\temp\wix")
 
-Write-Host "[INFO] Downloading PowerToys"
-Invoke-RestMethod "https://github.com/microsoft/PowerToys/releases/download/v0.76.2/PowerToysUserSetup-0.76.2-x64.exe" -OutFile "$PSScriptRoot\temp\PowerToysUserSetup.exe"
+# TODO: Update PowerToysUrl if there is a newer version of PowerToys with changes to FancyZones under: https://github.com/microsoft/PowerToys/releases
+$PowerToysUrl = "https://github.com/microsoft/PowerToys/releases/download/v0.77.0/PowerToysUserSetup-0.77.0-x64.exe"
 
-Write-Host "[INFO] Extracting PowerToys bundle"
-. "$PSScriptRoot\temp\wix\dark.exe" -x "$PSScriptRoot\temp\PowerToysUserSetup" "$PSScriptRoot\temp\PowerToysUserSetup.exe" | Out-Null
-
-Write-Host "[INFO] Extracting PowerToys MSI"
-Start-Process -FilePath msiexec -ArgumentList "/quiet /a $PSScriptRoot\temp\PowerToysUserSetup\AttachedContainer\PowerToysUserSetup-0.76.2-x64.msi /qn TARGETDIR=$PSScriptRoot\temp\PowerToysUserSetup\MSI" -Wait -PassThru | Out-Null
-
-Write-Host "[INFO] Moving extracted files to output location"
-$FancyZoneFiles = @()
-$FancyZoneFiles += "clrjit.dll"
-$FancyZoneFiles += "ControlzEx.dll"
-$FancyZoneFiles += "coreclr.dll"
-$FancyZoneFiles += "D3DCompiler_47_cor3.dll"
-$FancyZoneFiles += "DirectWriteForwarder.dll"
-$FancyZoneFiles += "hostfxr.dll"
-$FancyZoneFiles += "hostpolicy.dll"
-$FancyZoneFiles += "Ijwhost.dll"
-$FancyZoneFiles += "License.rtf"
-$FancyZoneFiles += "Microsoft.Win32.Primitives.dll"
-$FancyZoneFiles += "Microsoft.Win32.Registry.dll"
-$FancyZoneFiles += "Microsoft.Win32.SystemEvents.dll"
-$FancyZoneFiles += "Microsoft.Windows.SDK.NET.dll"
-$FancyZoneFiles += "ModernWpf.Controls.dll"
-$FancyZoneFiles += "ModernWpf.dll"
-$FancyZoneFiles += "msvcp140.dll"
-$FancyZoneFiles += "PowerToys.Common.UI.dll"
-$FancyZoneFiles += "PowerToys.FancyZones.exe"
-$FancyZoneFiles += "PowerToys.FancyZonesEditor.dll"
-$FancyZoneFiles += "PowerToys.FancyZonesEditor.exe"
-$FancyZoneFiles += "PowerToys.GPOWrapper.dll"
-$FancyZoneFiles += "PowerToys.GPOWrapperProjection.dll"
-$FancyZoneFiles += "PowerToys.Interop.dll"
-$FancyZoneFiles += "PowerToys.ManagedCommon.dll"
-$FancyZoneFiles += "PowerToys.ManagedTelemetry.dll"
-$FancyZoneFiles += "PresentationCore.dll"
-$FancyZoneFiles += "PresentationFramework-SystemCore.dll"
-$FancyZoneFiles += "PresentationFramework-SystemXml.dll"
-$FancyZoneFiles += "PresentationFramework.Aero2.dll"
-$FancyZoneFiles += "PresentationFramework.dll"
-$FancyZoneFiles += "PresentationNative_cor3.dll"
-$FancyZoneFiles += "System.Collections.Concurrent.dll"
-$FancyZoneFiles += "System.Collections.dll"
-$FancyZoneFiles += "System.Collections.NonGeneric.dll"
-$FancyZoneFiles += "System.Collections.Specialized.dll"
-$FancyZoneFiles += "System.ComponentModel.dll"
-$FancyZoneFiles += "System.ComponentModel.EventBasedAsync.dll"
-$FancyZoneFiles += "System.ComponentModel.Primitives.dll"
-$FancyZoneFiles += "System.ComponentModel.TypeConverter.dll"
-$FancyZoneFiles += "System.Configuration.ConfigurationManager.dll"
-$FancyZoneFiles += "System.Diagnostics.Debug.dll"
-$FancyZoneFiles += "System.Diagnostics.FileVersionInfo.dll"
-$FancyZoneFiles += "System.Diagnostics.Process.dll"
-$FancyZoneFiles += "System.Diagnostics.StackTrace.dll"
-$FancyZoneFiles += "System.Diagnostics.TextWriterTraceListener.dll"
-$FancyZoneFiles += "System.Diagnostics.TraceSource.dll"
-$FancyZoneFiles += "System.Diagnostics.Tracing.dll"
-$FancyZoneFiles += "System.Drawing.Primitives.dll"
-$FancyZoneFiles += "System.IO.Abstractions.dll"
-$FancyZoneFiles += "System.IO.Packaging.dll"
-$FancyZoneFiles += "System.Linq.dll"
-$FancyZoneFiles += "System.Linq.Expressions.dll"
-$FancyZoneFiles += "System.Memory.dll"
-$FancyZoneFiles += "System.Net.Primitives.dll"
-$FancyZoneFiles += "System.Net.Requests.dll"
-$FancyZoneFiles += "System.Net.WebClient.dll"
-$FancyZoneFiles += "System.Net.WebHeaderCollection.dll"
-$FancyZoneFiles += "System.Numerics.Vectors.dll"
-$FancyZoneFiles += "System.ObjectModel.dll"
-$FancyZoneFiles += "System.Private.CoreLib.dll"
-$FancyZoneFiles += "System.Private.Uri.dll"
-$FancyZoneFiles += "System.Private.Xml.dll"
-$FancyZoneFiles += "System.Reflection.Emit.dll"
-$FancyZoneFiles += "System.Reflection.Emit.ILGeneration.dll"
-$FancyZoneFiles += "System.Reflection.Emit.Lightweight.dll"
-$FancyZoneFiles += "System.Reflection.Primitives.dll"
-$FancyZoneFiles += "System.Runtime.CompilerServices.Unsafe.dll"
-$FancyZoneFiles += "System.Runtime.CompilerServices.VisualC.dll"
-$FancyZoneFiles += "System.Runtime.dll"
-$FancyZoneFiles += "System.Runtime.Extensions.dll"
-$FancyZoneFiles += "System.Runtime.InteropServices.dll"
-$FancyZoneFiles += "System.Runtime.Intrinsics.dll"
-$FancyZoneFiles += "System.Runtime.Loader.dll"
-$FancyZoneFiles += "System.Runtime.Serialization.Formatters.dll"
-$FancyZoneFiles += "System.Security.AccessControl.dll"
-$FancyZoneFiles += "System.Security.Cryptography.Algorithms.dll"
-$FancyZoneFiles += "System.Security.Cryptography.dll"
-$FancyZoneFiles += "System.Text.Encoding.Extensions.dll"
-$FancyZoneFiles += "System.Text.Encodings.Web.dll"
-$FancyZoneFiles += "System.Text.Json.dll"
-$FancyZoneFiles += "System.Threading.dll"
-$FancyZoneFiles += "System.Threading.Thread.dll"
-$FancyZoneFiles += "System.Threading.ThreadPool.dll"
-$FancyZoneFiles += "System.Windows.Controls.Ribbon.dll"
-$FancyZoneFiles += "System.Windows.Extensions.dll"
-$FancyZoneFiles += "System.Xaml.dll"
-$FancyZoneFiles += "System.Xml.ReaderWriter.dll"
-$FancyZoneFiles += "UIAutomationProvider.dll"
-$FancyZoneFiles += "UIAutomationTypes.dll"
-$FancyZoneFiles += "vcruntime140.dll"
-$FancyZoneFiles += "vcruntime140_1.dll"
-$FancyZoneFiles += "vcruntime140_cor3.dll"
-$FancyZoneFiles += "WindowsBase.dll"
-$FancyZoneFiles += "WindowsFormsIntegration.dll"
-$FancyZoneFiles += "WinRT.Runtime.dll"
-$FancyZoneFiles += "wpfgfx_cor3.dll"
-
-function ForceMoveItem([string]$Path, [string]$Destination) {
-    $DestinationDir = Split-Path $destination
-    if (!([System.IO.Directory]::Exists($DestinationDir))) { [System.IO.Directory]::CreateDirectory($DestinationDir) | Out-Null }
-    Copy-Item $path $destination
+function MoveItem([string]$Path, [string]$Destination) {
+    if (([System.IO.File]::Exists($Path))) {
+        $DestinationDir = Split-Path $Destination
+        if (!([System.IO.Directory]::Exists($DestinationDir))) { [System.IO.Directory]::CreateDirectory($DestinationDir) | Out-Null }
+        Copy-Item $Path $Destination
+    }
 }
 
-$FancyZoneFiles | foreach { ForceMoveItem "$PSScriptRoot\temp\PowerToysUserSetup\MSI\PowerToys\$($_)" "$PSScriptRoot\bin\$($_)" }
+Write-Host "[INFO] Starting deployment to $PSScriptRoot\bin using temp directory $PSScriptRoot\temp"
+if (!([System.IO.Directory]::Exists("$PSScriptRoot\temp"))) {
+    Write-Host "[INFO] Generating temp directory"
+    [System.IO.Directory]::CreateDirectory("$PSScriptRoot\temp") | Out-Null
+}
 
-Write-Host "[INFO] Helper script deployment"
-Copy-Item "$PSScriptRoot\GenerateDeployment.ps1" "$PSScriptRoot\bin\GenerateDeployment.ps1"
-Copy-Item "$PSScriptRoot\PowerToys.FancyZones.Configure.ps1" "$PSScriptRoot\bin\PowerToys.FancyZones.Configure.ps1"
-Copy-Item "$PSScriptRoot\PowerToys.FancyZones.Restart.ps1" "$PSScriptRoot\bin\PowerToys.FancyZones.Restart.ps1"
+if (!([System.IO.File]::Exists("$PSScriptRoot\temp\wix.zip"))) {
+    Write-Host "[INFO] Downloading Wix"
+    Invoke-RestMethod "https://github.com/wixtoolset/wix3/releases/download/wix3112rtm/wix311-binaries.zip" -OutFile "$PSScriptRoot\temp\wix.zip"
+}
 
-Write-Host "[INFO] Generating deployment .zip"
-[System.IO.Compression.ZipFile]::CreateFromDirectory("$PSScriptRoot\bin", "$PSScriptRoot\PowerToys.FancyZones.zip")
+if (!([System.IO.File]::Exists("$PSScriptRoot\temp\ProcessMonitor.zip"))) {
+    Write-Host "[INFO] Downloading ProcessMonitor"
+    Invoke-RestMethod "https://download.sysinternals.com/files/ProcessMonitor.zip" -OutFile "$PSScriptRoot\temp\ProcessMonitor.zip"
+}
+if (!([System.IO.Directory]::Exists("$PSScriptRoot\temp\wix"))) {
+    Write-Host "[INFO] Extracting Wix"
+    [System.IO.Compression.ZipFile]::ExtractToDirectory("$PSScriptRoot\temp\wix.zip", "$PSScriptRoot\temp\wix")
+}
+
+if (!([System.IO.Directory]::Exists("$PSScriptRoot\temp\ProcessMonitor"))) {
+    Write-Host "[INFO] Extracting ProcessMonitor"
+    [System.IO.Compression.ZipFile]::ExtractToDirectory("$PSScriptRoot\temp\ProcessMonitor.zip", "$PSScriptRoot\temp\ProcessMonitor")
+}
+
+if (!([System.IO.File]::Exists("$PSScriptRoot\temp\PowerToysUserSetup.exe"))) {
+    Write-Host "[INFO] Downloading PowerToys"
+    Invoke-RestMethod $PowerToysUrl -OutFile "$PSScriptRoot\temp\PowerToysUserSetup.exe"
+}
+
+if (!([System.IO.Directory]::Exists("$PSScriptRoot\temp\PowerToysUserSetup"))) {
+    Write-Host "[INFO] Extracting PowerToys bundle"
+    . "$PSScriptRoot\temp\wix\dark.exe" -x "$PSScriptRoot\temp\PowerToysUserSetup" "$PSScriptRoot\temp\PowerToysUserSetup.exe" | Out-Null
+}
+
+if (!([System.IO.Directory]::Exists("$PSScriptRoot\temp\PowerToysUserSetup\MSI"))) {
+    Write-Host "[INFO] Extracting PowerToys MSI"
+    $MsiPath = Get-ChildItem "$PSScriptRoot\temp\PowerToysUserSetup\AttachedContainer\PowerToysUserSetup-*-x64.msi" | foreach { $_.FullName }
+    Start-Process -FilePath msiexec -ArgumentList "/quiet /a $MsiPath /qn TARGETDIR=$PSScriptRoot\temp\PowerToysUserSetup\MSI" -Wait -PassThru | Out-Null
+}
+
+if (!([System.IO.Directory]::Exists("$PSScriptRoot\bin"))) {
+    Write-Host "[INFO] Moving extracted files to output location"
+    $FancyZonesFiles = Get-Content "$PSScriptRoot\FancyZonesDependencies.txt"
+    $FancyZonesFiles += "PowerToys.FancyZones.exe"
+    $FancyZonesFiles += "PowerToys.FancyZonesEditor.exe"
+    $FancyZonesFiles | foreach { MoveItem "$PSScriptRoot\temp\PowerToysUserSetup\MSI\PowerToys\$($_)" "$PSScriptRoot\bin\$($_)" }
+
+    Write-Host "[INFO] Helper script deployment"
+    Copy-Item "$PSScriptRoot\README.md" "$PSScriptRoot\bin\README.md"
+    Copy-Item "$PSScriptRoot\FancyZonesDependencies.txt" "$PSScriptRoot\bin\FancyZonesDependencies.txt"
+    Copy-Item "$PSScriptRoot\GenerateDeployment.ps1" "$PSScriptRoot\bin\GenerateDeployment.ps1"
+    Copy-Item "$PSScriptRoot\PowerToys.FancyZones.Configure.ps1" "$PSScriptRoot\bin\PowerToys.FancyZones.Configure.ps1"
+    Copy-Item "$PSScriptRoot\PowerToys.FancyZones.Restart.ps1" "$PSScriptRoot\bin\PowerToys.FancyZones.Restart.ps1"
+}
+
+if (!([System.IO.File]::Exists("$PSScriptRoot\PowerToys.FancyZones.zip"))) {
+    Write-Host "[INFO] Generating deployment .zip"
+    [System.IO.Compression.ZipFile]::CreateFromDirectory("$PSScriptRoot\bin", "$PSScriptRoot\PowerToys.FancyZones.zip")
+}
+
+Write-Host "[INFO] Deployment generated under $PSScriptRoot\PowerToys.FancyZones.zip"
 Get-FileHash "$PSScriptRoot\PowerToys.FancyZones.zip" | foreach { Write-Host "[INFO] Deployment .zip SHA256 hash : $($_.Hash)" }
--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # FancyZones
 Custom https://github.com/microsoft/PowerToys re-deployment to run FancyZones standalone
+
+# Creating a new deployment
+- Open cmd.exe in Git repository location
+- Run the `GenerateDeployment.ps1` using command: `powershell -ExecutionPolicy Bypass -File GenerateDeployment.ps1`
+
+# Running the deployment locally
+- Download the deployment ZIP from `https://github.com/rolandas-rimkus/FancyZones/releases`
+- Extract locally and run `PowerToys.FancyZones.Configure.ps1` to setup the required registry keys and configuration files required for FancyZones to run.
+- Run `PowerToys.FancyZonesEditor.exe` to configure the layouts as normal
+- Start `PowerToys.FancyZones.exe` manually or restart your computer as the process is setup to start on startup in the background
+- Note: once you modify the layout using `PowerToys.FancyZonesEditor.exe` the `PowerToys.FancyZones.exe` running in the background needs to be restarted to pick up the changes.
+  Use `PowerToys.FancyZones.Restart.ps1` to do so or manually kill the `PowerToys.FancyZones.exe` using Task Manager and run it again.
+
+# Updating to latest version of PowerToys
+- Update PowerToysUrl under GenerateDeployment.ps1 to latest PowerToys with changes to FancyZones
+- Run `GenerateDeployment.ps1` using command: `powershell -ExecutionPolicy Bypass -File GenerateDeployment.ps1`
+- Run `temp\PowerToysUserSetup\MSI\PowerToys\PowerToys.exe`
+- Configure all the FancyZones related settings using the provided UI
+- Go to `%localappdata%\Microsoft\PowerToys\FancyZones` using file explorer
+- Open `settings.json` and `editor-parameters.json`. Update `PowerToys.FancyZones.Configure.ps1` to reflect the settings PowerToys.exe sets for FancyZones to run properly.
+- Run `temp\ProcessMonitor\Procmon64.exe`
+- Filter down the events to only include events from `PowerToys.FancyZones.exe` and `PowerToys.FancyZonesEditor.exe`
+- Make sure `temp\PowerToysUserSetup\MSI\PowerToys\PowerToys.exe` is not running anymore in the background
+- Run `temp\PowerToysUserSetup\MSI\PowerToys\PowerToys.FancyZonesEditor.exe`
+- Create new layout and enable it
+- Run `temp\PowerToysUserSetup\MSI\PowerToys\PowerToys.FancyZones.exe`
+- Drag windows to the created layout to test that Fancy Zones works as expected
+- Make sure `temp\PowerToysUserSetup\MSI\PowerToys\PowerToys.FancyZones.exe` is not running anymore in the background
+- Check ProcMon output and export the accessed file list using `Tools` > `File Summary`. Update the `FancyZonesFiles.txt` with new list of files that fancy zones accessed during runtime.
+  This list will be used to only deliver the binaries that Fancy Zones requires to work instead of the full PowerToys deployment.
+- Delete `temp`, `bin`, `PowerToys.FancyZones.zip` and rerun the deployment
+- Test running the FancyZones from the `bin` folder. If everything works properly deploy the ZIP as a new release in GitHub
